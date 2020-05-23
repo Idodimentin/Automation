2cbf4db67ad1ab28b3d617935851b7259cef8827
idodimentin <ido.131199@gmail.com>
2020-05-23 14:09:55 +0300

    useing return on func

--- pythonBasics.py
+++ pythonBasics.py
@@ -1,21 +1,28 @@
-def multiplication():
-    x = int(input("enter the 1st number here:"))    
-    y = int(input("enter the 2nd number here:"))
-    print(x*y)
-#multiplication()
+x = int(input("enter the 1st number here:"))    
+y = int(input("enter the 2nd number here:"))
 
-def missionTwo():
-    num1 = int(input("enter a base number here:"))    
-    num2 = int(input("enter a power number here:"))
-    print(num1**num2)
-#missionTwo()
+def multiplication(x, y):
+    z = x * y
+    return z 
+multiplication(x, y)
+print(multiplication(x, y)) 
 
-def missionThree():
-    num3 = int(input("enter a number here:"))    
-    num4 = int(input("enter a number here:"))
+num1 = int(input("enter a base number here:"))    
+num2 = int(input("enter a power number here:"))
+
+def missionTwo(num1, num2):
+    result = num1 ** num2
+    return result
+missionTwo(num1, num2)    
+print(missionTwo(num1, num2))
+
+num3 = int(input("enter a number here:"))    
+num4 = int(input("enter a number here:"))
+
+def missionThree(num3, num4):
     multiply = 0
     for i in range(1, num4+1):
         multiply = multiply + num3
         print("last result is the multiplication of 2 number: ", multiply)
 
-missionThree()
+missionThree(num3, num4)
